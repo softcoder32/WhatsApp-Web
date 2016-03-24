13a65a676ba26c7129324dab053fe7069b9249b6
Collins Nji <collins.geek@gmail.com>
2016-03-24 21:45:27 +0100

    javascript to handle run-time properties

---
+++ config.js
@@ -0,0 +1,11 @@
+window.config = {
+   "model_": "AppConfig",
+   "id": 1,
+   "appName": "WhatsApp Web",
+   "homepage": "https://web.whatsapp.com",
+   "enableNavBttns": false,
+   "enableHomeBttn": false,
+   "enableReloadBttn": false,
+   "enableLogoutBttn": false,
+   "kioskEnabled": false
+};
